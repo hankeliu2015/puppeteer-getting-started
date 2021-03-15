2499e287d380d42120483ed58105523dbeda2ea2
Hank Liu <hanke.liu@gmail.com>
2021-03-15 16:22:37 -0400

    automate login testing

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const puppeteer = require('puppeteer');
+const CREDS = require('./creds.js');
+
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   
-  await page.goto('https://github.com');
-  await page.screenshot({ path: 'screenshots/github.png' });
+  await page.goto('https://github.com/login');
+  //dom element selectors
+  const USERNAME_SELECTOR = '#login_field';
+  const PASSWORD_SELECTOR = '#password';
+  const BUTTON_SELECTOR = '#login > div.auth-form-body.mt-3 > form > div > input.btn.btn-primary.btn-block'
+
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(CREDS.username);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(CREDS.password);
+  debugger
+  await page.click(BUTTON_SELECTOR);
+
+  await page.waitForNavigation();
+
+  const userToSearch = 'john';
+  const searchUrl = `https://github.com/search?q=${userToSearch}&type=Users&utf8=%E2%9C%93`;
+
+  await page.goto(searchUrl);
+  await page.waitFor(2*1000);
+
+  // await page.screenshot({ path: 'screenshots/github.png' });
   
-  browser.close();
+  // browser.close();
 }
 
 run();
